ec2b050668a706012ec0f2ec538b69a026a6efb0
DESKTOP-6UEB27H\Salgado <sebastesb1986@gmail.com>
2025-03-20 14:25:11 -0500

    agregación a vite.config 5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title></title>
-        @vite('resources/css/app.css')
+        <title>HOLA</title>
+
     </head>
     <body>
-        <div id="app"></div>
-        @vite('resources/js/app.js')
+        <BUtton>aqui</BUtton>
     </body>
 </html>
